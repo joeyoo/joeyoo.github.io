935f4fc241e577f937429715681cb613c26ec421
Joseph Yoo <joseph_yoo@brown.edu>
2018-12-11 03:55:09 -0500

    Updates

---
+++ precache-manifest.fc45a9ddd1c12c835297b7ebc5e46b30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c19be11a7f5cadd447ff697212386ad",
+    "url": "/static/media/portrait.6c19be11.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c52adebeaa1ccb3e180",
+    "url": "/static/js/main.6c52adeb.chunk.js"
+  },
+  {
+    "revision": "be1b7e0bb9b97a940954",
+    "url": "/static/js/1.be1b7e0b.chunk.js"
+  },
+  {
+    "revision": "6c52adebeaa1ccb3e180",
+    "url": "/static/css/main.777d8134.chunk.css"
+  },
+  {
+    "revision": "3a3ef576c2b21756964bc541516a7ded",
+    "url": "/index.html"
+  }
+];
