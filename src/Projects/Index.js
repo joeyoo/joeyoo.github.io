f83308096444d22a56e7b7ab33df31d4f0135f97
Joseph Yoo <joseph_yoo@brown.edu>
2018-12-12 22:11:08 -0500

    prep for push

--- src/Projects/Index.js
+++ src/Projects/Index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 class Projects extends Component {
 	constructor(props) {
 		super(props);
--- src/Projects/SideMenu.js
+++ src/Projects/SideMenu.js
@@ -1,77 +1,42 @@
 import React, { Component } from 'react'
-import { Accordion, Form, Menu, Step, Icon } from 'semantic-ui-react'
+import { Accordion, Form, Menu, Step, Icon, Rail, Segment } from 'semantic-ui-react'
 
-const SizeForm = (
-  <Step.Group vertical fluid>
-    <Step>
-      <Step.Content>
-        <Step.Title>Education</Step.Title>
-      </Step.Content>
-    </Step>
+export default class SideMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    <Step>
-      <Step.Content>
-        <Step.Title>Professional Experience</Step.Title>
-      </Step.Content>
-    </Step>
+  handleClick = (e) => {
+    const index = parseInt(e.target.id);
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
 
-    <Step active>
-      <Step.Content>
-        <Step.Title>Projects</Step.Title>
-      </Step.Content>
-    </Step>
-  </Step.Group>
-)
-
-
-export default class AccordionExampleMenu extends Component {
-  state = { activeIndex: 0 }
-
-  handleClick = (e, titleProps) => {
-    const { index } = titleProps
-    const { activeIndex } = this.state
-    const newIndex = activeIndex === index ? -1 : index
-
-    this.setState({ activeIndex: newIndex })
+    this.setState({ activeIndex: newIndex });
   }
 
   render() {
-    const { activeIndex } = this.state
+    const { activeIndex } = this.state;
 
     return (
       <Step.Group fluid vertical>
-
-        <Step>
-          <Icon name='id card outline'/>
+        <Step active={activeIndex === 0} id={0} onClick={this.handleClick} >
+          <Icon name='id card outline' id={0} onClick={this.handleClick} />
           <Step.Content>
-            <Step.Title>Cover Letter</Step.Title>
+            <Step.Title id={0} onClick={this.handleClick} >Cover Letter</Step.Title>
           </Step.Content>
         </Step>
 
-        <Step>
-          <Icon name='wordpress forms' />
+        <Step active={activeIndex === 1} id={1} onClick={this.handleClick} >
+          <Icon name='wordpress forms' id={1} onClick={this.handleClick} />
           <Step.Content>
-            <Step.Title>Resume</Step.Title>
+            <Step.Title id={1} onClick={this.handleClick}>Resume</Step.Title>
           </Step.Content>
         </Step>
-
-        <Step active>
-          <Icon name='wordpress forms' />
-          <Step.Content>
-            <Step.Title>Resume</Step.Title>
-            <Accordion vertical>
-                <Accordion.Title
-                  active={activeIndex === 2}
-                  index={0}
-                  onClick={this.handleClick}
-                />
-                <Accordion.Content active={activeIndex === 0} content={SizeForm} />
-            </Accordion>
-          </Step.Content>
-        </Step>
-
       </Step.Group>
-
     )
   }
 }
