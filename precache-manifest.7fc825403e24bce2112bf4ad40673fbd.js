ccf4de706396b189bd51d68ba4224b5f21527695
Joseph Yoo <notjoeyoo@gmail.com>
2020-01-05 12:51:07 -0500

    Updates

---
+++ precache-manifest.7fc825403e24bce2112bf4ad40673fbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bc812282291fa5ffb2",
+    "url": "/static/css/main.b6acd185.chunk.css"
+  },
+  {
+    "revision": "69bc812282291fa5ffb2",
+    "url": "/static/js/main.1edd9676.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c36e1e40ac3409bd7471",
+    "url": "/static/js/2.32510b3f.chunk.js"
+  },
+  {
+    "revision": "706cccf2d540be05bfeef674c17e3218",
+    "url": "/index.html"
+  }
+];
