eec4797e2aef790b09c9e9815b87962da14ec7a2
Joseph Yoo <joseph_yoo@brown.edu>
2020-01-05 11:12:22 -0500

    update routing

--- src/1_Auth/index.js
+++ src/1_Auth/index.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react';
-import request from 'request-promise';
-import {ServerURL} from '../0_config';
-import {withRouter} from 'react-router-dom';
-import { Button, Form, Placeholder, Reveal, Grid, Icon, Popup, Container, Item, Image} from 'semantic-ui-react';
+import React, { Component } from "react";
+import request from "request-promise";
+import { ServerURL } from "../0_config";
+import { withRouter } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Placeholder,
+  Reveal,
+  Grid,
+  Icon,
+  Popup,
+  Container,
+  Item,
+  Image
+} from "semantic-ui-react";
 
-const GateKeeper = ({active, disabled, eye, eye2, ...props}) => (
-  <Reveal active={active} disabled={disabled} animated="move" style={{width: "75px"}} >
+const GateKeeper = ({ active, disabled, eye, eye2, ...props }) => (
+  <Reveal
+    active={active}
+    disabled={disabled}
+    animated="move"
+    style={{ width: "75px" }}
+  >
     <Reveal.Content visible>
-        <div style={{backgroundColor: "#282c34", height: "50px", width: "75px", border: "inset 1px #212630"}}></div>
+      <div
+        style={{
+          backgroundColor: "#282c34",
+          height: "50px",
+          width: "75px",
+          border: "inset 1px #212630"
+        }}
+      />
     </Reveal.Content>
     <Reveal.Content hidden>
-      <div className='eyeContainer'>
-        <div id={eye}></div>
-        <div id={eye2}></div>
-        <path d="M128.21,71H75.79C73.71,71,72,68.87,72,66.26v-.52C72,63.13,73.71,61,75.79,61h52.42c2.08,0,3.79,2.13,3.79,4.74v.52C132,68.87,130.29,71,128.21,71Z" id="brows"></path>
+      <div className="eyeContainer">
+        <div id={eye} />
+        <div id={eye2} />
+        <path
+          d="M128.21,71H75.79C73.71,71,72,68.87,72,66.26v-.52C72,63.13,73.71,61,75.79,61h52.42c2.08,0,3.79,2.13,3.79,4.74v.52C132,68.87,130.29,71,128.21,71Z"
+          id="brows"
+        />
       </div>
     </Reveal.Content>
   </Reveal>
 );
 
 const transform = (ele, name, value, unit) => {
-  ele.style.transform = name + '(' + value + unit + ')';
-  ele.style.webkitTransform = name + '(' + value + unit + ')';
-  ele.style.mozTransform = name + '(' + value + unit + ')';
-  ele.style.msTransform = name + '(' + value + unit + ')';
-  ele.style.oTransform = name + '(' + value + unit + ')';
-}
+  ele.style.transform = name + "(" + value + unit + ")";
+  ele.style.webkitTransform = name + "(" + value + unit + ")";
+  ele.style.mozTransform = name + "(" + value + unit + ")";
+  ele.style.msTransform = name + "(" + value + unit + ")";
+  ele.style.oTransform = name + "(" + value + unit + ")";
+};
 
 class Auth extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {value: '', active: false, disabled: true, alreadyAuthenticated: false, authorized: false};
+    this.state = {
+      value: "",
+      active: false,
+      disabled: true,
+      alreadyAuthenticated: false,
+      authorized: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -41,41 +73,65 @@ class Auth extends Component {
 
   async _checkToken() {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (!token) return;
 
-      const res = await request({method: "GET", url: ServerURL, json: true, qs: {access_token: token}});
+      const res = await request({
+        method: "GET",
+        url: ServerURL,
+        json: true,
+        qs: { access_token: token }
+      });
       if (res.auth === true) {
-        this.setState({active: true, authorized: true});
+        this.setState({ active: true, authorized: true });
         const t = this;
         setTimeout(function() {
-          t.setState({active: false});
+          t.setState({ active: false });
         }, 1500);
         setTimeout(function() {
-          t.props.history.push({pathname: '/projects', state: {authorized: true}});
+          t.props.history.push({
+            pathname: "/projects",
+            state: { authorized: true }
+          });
         }, 2200);
       }
-    }
-    catch(err) {
+    } catch (err) {
       console.error(err);
     }
   }
 
   handleChange(e) {
-    this.setState({value: e.target.value});
+    this.setState({ value: e.target.value });
     var add = e.target.selectionStart,
-        charWidth = parseInt(window.getComputedStyle(e.target).getPropertyValue("font-size")) * 0.15,
-        x = (add * charWidth);
+      charWidth =
+        parseInt(
+          window.getComputedStyle(e.target).getPropertyValue("font-size")
+        ) * 0.15,
+      x = add * charWidth;
 
     var eye = document.getElementById("eye"),
-        eye2 = document.getElementById("eye2");
+      eye2 = document.getElementById("eye2");
 
     var left = e.target.parentNode.getBoundingClientRect().left,
-        rot1 = (Math.atan2((left + x) - eye.getBoundingClientRect().left, e.target.getBoundingClientRect().top - eye.getBoundingClientRect().top) * (180 / Math.PI) * -1) + 180,
-        rot2 = (Math.atan2((left + x) - eye.getBoundingClientRect().left, e.target.getBoundingClientRect().top - eye.getBoundingClientRect().top) * (180 / Math.PI) * -1) + 180;
+      rot1 =
+        Math.atan2(
+          left + x - eye.getBoundingClientRect().left,
+          e.target.getBoundingClientRect().top - eye.getBoundingClientRect().top
+        ) *
+          (180 / Math.PI) *
+          -1 +
+        180,
+      rot2 =
+        Math.atan2(
+          left + x - eye.getBoundingClientRect().left,
+          e.target.getBoundingClientRect().top - eye.getBoundingClientRect().top
+        ) *
+          (180 / Math.PI) *
+          -1 +
+        180;
 
-    transform(eye, 'rotate', rot1, 'deg');
-    transform(eye2, 'rotate', rot2, 'deg');
+    transform(eye, "rotate", rot1, "deg");
+    transform(eye2, "rotate", rot2, "deg");
   }
 
   handleFocus(event) {
@@ -83,19 +139,42 @@ class Auth extends Component {
       active: true
     }));
     var eye = document.getElementById("eye"),
-        eye2 = document.getElementById("eye2"),
-        rotI = (Math.atan2(event.target.getBoundingClientRect().left - eye.getBoundingClientRect().left, event.target.getBoundingClientRect().top - eye.getBoundingClientRect().top) * (180 / Math.PI) * -1) + 180;
+      eye2 = document.getElementById("eye2"),
+      rotI =
+        Math.atan2(
+          event.target.getBoundingClientRect().left -
+            eye.getBoundingClientRect().left,
+          event.target.getBoundingClientRect().top -
+            eye.getBoundingClientRect().top
+        ) *
+          (180 / Math.PI) *
+          -1 +
+        180;
 
-    transform(eye, 'rotate', rotI, 'deg');
-    transform(eye2, 'rotate', rotI, 'deg');
+    transform(eye, "rotate", rotI, "deg");
+    transform(eye2, "rotate", rotI, "deg");
 
     document.body.onmousemove = function(e) {
-      var rot = (Math.atan2(e.pageX - eye.getBoundingClientRect().left, e.pageY - eye.getBoundingClientRect().top) * (180 / Math.PI) * -1) + 180,
-          rot2 = (Math.atan2(e.pageX - eye2.getBoundingClientRect().left, e.pageY - eye2.getBoundingClientRect().top) * (180 / Math.PI) * -1) + 180;
+      var rot =
+          Math.atan2(
+            e.pageX - eye.getBoundingClientRect().left,
+            e.pageY - eye.getBoundingClientRect().top
+          ) *
+            (180 / Math.PI) *
+            -1 +
+          180,
+        rot2 =
+          Math.atan2(
+            e.pageX - eye2.getBoundingClientRect().left,
+            e.pageY - eye2.getBoundingClientRect().top
+          ) *
+            (180 / Math.PI) *
+            -1 +
+          180;
 
-      transform(eye, 'rotate', rot, 'deg');
-      transform(eye2, 'rotate', rot2, 'deg');
-    }
+      transform(eye, "rotate", rot, "deg");
+      transform(eye2, "rotate", rot2, "deg");
+    };
   }
 
   handleBlur(e) {
@@ -109,49 +188,70 @@ class Auth extends Component {
   async handleSubmit(event) {
     event.preventDefault();
     try {
-      const res = await request({method: "POST", uri: ServerURL, json: true, body: {password: this.state.value}});
+      const res = await request({
+        method: "POST",
+        uri: ServerURL,
+        json: true,
+        body: { password: this.state.value }
+      });
       if (res.auth === true) {
-        this.setState({active: true, authorized: true});
+        this.setState({ active: true, authorized: true });
         const t = this;
         setTimeout(function() {
-          t.setState({active: false});
+          t.setState({ active: false });
         }, 1500);
         setTimeout(function() {
-          t.props.history.push({pathname: '/projects', state: {authorized: true}});
+          t.props.history.push({
+            pathname: "/projects",
+            state: { authorized: true }
+          });
         }, 2200);
-        localStorage.setItem('token', res.token);
+        localStorage.setItem("token", res.token);
       }
-    }
-    catch(err) {
-      this.setState({value: ''});
+    } catch (err) {
+      this.setState({ value: "" });
     }
   }
 
   render() {
-    const {Group, Input} = Form;
+    const { Group, Input } = Form;
     const eye = this.state.authorized === true ? "eyeC" : "eye",
-          eye2 = this.state.authorized === true ? "eye2C" : "eye2";
+      eye2 = this.state.authorized === true ? "eye2C" : "eye2";
     return (
-        <div>
-            <Popup trigger={<Icon name='help' className='help circle'/>} hoverable position='left center'>
-                <Item>
-                  <Item.Content>
-                    <Item.Description>
-                      If this is for an application, you'll find the password near the top of my attached resume!
-                    </Item.Description>
-                  </Item.Content>
-                </Item>
-            </Popup>
+      <div>
+        <Popup
+          trigger={<Icon name="help" className="help circle" />}
+          hoverable
+          position="left center"
+        >
+          <Item>
+            <Item.Content>
+              <Item.Description>Password is: onward!</Item.Description>
+            </Item.Content>
+          </Item>
+        </Popup>
 
-            <GateKeeper active={this.state.active} disabled={this.state.disabled} eye={eye} eye2={eye2}/>
-            <Form onSubmit={this.handleSubmit} autoComplete="off">
-              <Group>
-                <Input onFocus={this.handleFocus} onBlur={this.handleBlur} type="password" name="password" value={this.state.value} onChange={this.handleChange} placeholder="Secret word?"/>
-                <Button type='submit'>Submit</Button>
-              </Group>
-            </Form>
-        </div>
-
+        <GateKeeper
+          active={this.state.active}
+          disabled={this.state.disabled}
+          eye={eye}
+          eye2={eye2}
+        />
+        <Form onSubmit={this.handleSubmit} autoComplete="off">
+          <Group>
+            <Input
+              onFocus={this.handleFocus}
+              onBlur={this.handleBlur}
+              type="password"
+              name="password"
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder="Secret word?"
+            />
+            <Button type="submit">Submit</Button>
+          </Group>
+        </Form>
+      </div>
     );
   }
 }
--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route as BrowserRoute, Redirect, withRouter, Switch } from "react-router-dom";
-import {Grid, Container, Button, Sticky} from 'semantic-ui-react';
-import request from 'request-promise';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route as BrowserRoute,
+  Redirect,
+  withRouter,
+  Switch
+} from "react-router-dom";
+import { Grid, Container, Button, Sticky } from "semantic-ui-react";
+import request from "request-promise";
 
-import {ServerURL} from './0_config';
-import Auth from './1_Auth';
-import Projects from './1_Projects';
-import Home from './1_Home';
-
+import { ServerURL } from "./0_config";
+import Auth from "./1_Auth";
+import Projects from "./1_Projects";
+import Home from "./1_Home";
 
 class AuthRoute extends Component {
   constructor(props) {
     super(props);
     this.state = {
       authorized: props.authorized || false,
-      path: props.path || '/',
+      path: props.path || "/",
       component: props.component || null,
       authToAuth: false
     };
@@ -22,15 +27,16 @@ class AuthRoute extends Component {
 
   static getDerivedStateFromProps(props, state) {
     const pAuth = props.authorized,
-          pPath = props.location.pathname,
-          sAuth = state.authorized,
-          sPath = state.path,
-          newState = {};
+      pPath = props.location.pathname,
+      sAuth = state.authorized,
+      sPath = state.path,
+      newState = {};
 
     if (pAuth === sAuth && pPath === sPath) return null;
     if (pAuth !== sAuth) newState.authorized = pAuth;
     if (pPath !== sPath) newState.path = pPath;
-    if (pPath === '/auth' && sPath === '/auth' && pAuth === true) newState.authToAuth = true;
+    if (pPath === "/auth" && sPath === "/auth" && pAuth === true)
+      newState.authToAuth = true;
     return newState;
   }
 
@@ -39,17 +45,23 @@ class AuthRoute extends Component {
     return (
       <BrowserRoute
         {...p}
-        render={ props =>
-          this.state.authorized && !this.state.authToAuth
-            ? (<Component {...props} />)
-            : ((this.state.authToAuth && this.state.authorized)
-              ? (<Redirect to={{
-                pathname: '/projects',
+        render={props =>
+          this.state.authorized && !this.state.authToAuth ? (
+            <Component {...props} />
+          ) : this.state.authToAuth && this.state.authorized ? (
+            <Redirect
+              to={{
+                pathname: "/projects",
                 state: {
                   referrer: props.path,
                   authorized: props.authorized
-                }}} from={this.state.path} />)
-              : (<Redirect to="auth" from={this.state.path} />))
+                }
+              }}
+              from={this.state.path}
+            />
+          ) : (
+            <Redirect to="auth" from={this.state.path} />
+          )
         }
       />
     );
@@ -63,7 +75,7 @@ const Route = withRouter(AuthRoute);
 export default class Router extends Component {
   constructor(props) {
     super(props);
-    this.state = {authorized: false};
+    this.state = { authorized: false };
     this._checkToken();
   }
 
@@ -73,30 +85,34 @@ export default class Router extends Component {
 
   async _checkToken() {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (!token) return;
 
-      const res = await request({method: "GET", url: ServerURL, json: true, qs: {access_token: token}});
-      this.setState({authorized: res.auth});
-    }
-    catch(err) {
+      const res = await request({
+        method: "GET",
+        url: ServerURL,
+        json: true,
+        qs: { access_token: token }
+      });
+      this.setState({ authorized: res.auth });
+    } catch (err) {
       console.error(err);
     }
   }
 
   render() {
-    const {authorized} = this.state;
+    const { authorized } = this.state;
     return (
-        <BrowserRouter>
-          <Switch>
-            <BrowserRoute path="/" component={Home} />
-            <BrowserRoute path="/auth" component={Auth}/>
-            <Route path={"/projects"}
-                 component={Projects}
-                 authorized={authorized} />
-            <Route component={Auth}/>
-          </Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path={"/projects"}
+            component={Projects}
+            authorized={authorized}
+          />
+          <Route component={Auth} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
